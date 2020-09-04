96b2cbe00ab62f24a9f8afd1539a61226ec753d0
kolya <kolyabolt@yandex.ru>
2020-09-04 13:18:36 +0300

    _

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello world");
+        System.out.println("https://NikoBolt.github.io/Design-Patterns");
     }
 }
