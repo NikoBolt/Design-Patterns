a070038007158259656c1c2027eb44074fdf0ca7
kolya <kolyabolt@yandex.ru>
2020-09-07 20:58:11 +0300

    Singleton

---
+++ src/com/company/_5_Singletone/_2/Singleton.java
@@ -0,0 +1,18 @@
+package com.company._5_Singletone._2;
+
+public class Singleton {
+    private static Singleton uniqueInstance;
+
+    // other useful instance variables here
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+
+    // Другие методы
+}
