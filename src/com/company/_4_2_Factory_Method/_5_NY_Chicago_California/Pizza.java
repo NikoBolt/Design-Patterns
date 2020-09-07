8baeaa9082cb243ce670610698868a66a824eff0
kolya <kolyabolt@yandex.ru>
2020-09-07 19:43:39 +0300

    +

--- src/com/company/_4_2_Factory_Method/_5_NY_Chicago_California/Pizza.java
+++ src/com/company/_4_2_Factory_Method/_5_NY_Chicago_California/Pizza.java
@@ -14,6 +14,12 @@ abstract public class Pizza {
 
 	public void prepare() {
 		System.out.println("Preparing " + name);
+		System.out.println("Tossing dough...");
+		System.out.println("Adding sauce...");
+		System.out.println("Adding toppings: ");
+		for (String topping : toppings) {
+			System.out.println("   " + topping);
+		}
 	}
 
 	public void bake() {
