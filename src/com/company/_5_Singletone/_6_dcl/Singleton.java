39e8dded713f840196b889dd6e16b18f5644a12d
NikoBolt <docnikbolt@gmail.com>
2020-09-09 14:44:36 +0300

    Высокопроизводительная реализация. Синхронизация только при первом вызове

---
+++ src/com/company/_5_Singletone/_6_dcl/Singleton.java
@@ -0,0 +1,28 @@
+package com.company._5_Singletone._6_dcl;
+
+//
+// Danger!  This implementation of Singleton not
+// guaranteed to work prior to Java 5
+//
+
+public class Singleton {
+
+    // высокопроизводительная реализация
+
+    private volatile static Singleton uniqueInstance;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (uniqueInstance == null) {
+            /* синхронизация замедляет код в 100 и более раз
+            будет при первом вызове */
+            synchronized (Singleton.class) /**/ {
+                if (uniqueInstance == null) {
+                    uniqueInstance = new Singleton();
+                }
+            }
+        }
+        return uniqueInstance;
+    }
+}
---
+++ src/com/company/_5_Singletone/_6_dcl/SingletonClient.java
@@ -0,0 +1,7 @@
+package com.company._5_Singletone._6_dcl;
+
+public class SingletonClient {
+	public static void main(String[] args) {
+		Singleton singleton = Singleton.getInstance();
+	}
+}
