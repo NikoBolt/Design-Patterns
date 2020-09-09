a06533ea97be0feb0034fea15dc779ab6bb60fe0
NikoBolt <docnikbolt@gmail.com>
2020-09-09 14:39:36 +0300

    Многопоточный синглтон

---
+++ src/com/company/_5_Singletone/_4_1_Threadsafe/Singleton.java
@@ -0,0 +1,23 @@
+package com.company._5_Singletone._4_1_Threadsafe;
+
+public class Singleton {
+    private static Singleton uniqueInstance;
+
+    // other useful instance variables here
+    // Другие переменные экземпляра
+
+    private Singleton() {}
+
+    public static /* синхронизация замедляет код в 100 и более раз */ synchronized /**/ Singleton getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+
+    // Другие методы
+    // other useful methods here
+    public String getDescription() {
+        return "I'm a thread safe Singleton!";
+    }
+}
---
+++ src/com/company/_5_Singletone/_4_1_Threadsafe/SingletonClient.java
@@ -0,0 +1,8 @@
+package com.company._5_Singletone._4_1_Threadsafe;
+
+public class SingletonClient {
+	public static void main(String[] args) {
+		Singleton singleton = Singleton.getInstance();
+		System.out.println(singleton.getDescription());
+	}
+}
