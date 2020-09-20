0a85371c0fef7253fc58ee47f15ac73248b51dc8
NikoBolt <docnikbolt@gmail.com>
2020-09-20 19:54:13 +0300

    Тест кода без использования шаблона

---
+++ src/com/company/_8_TemplateMethod/_2_simplebarista/Barista.java
@@ -0,0 +1,13 @@
+package com.company._8_TemplateMethod._2_simplebarista;
+
+public class Barista {
+ 
+	public static void main(String[] args) {
+		Tea tea = new Tea();
+		Coffee coffee = new Coffee();
+		System.out.println("Making tea...");
+		tea.prepareRecipe();
+		System.out.println("Making coffee...");
+		coffee.prepareRecipe();
+	}
+}
