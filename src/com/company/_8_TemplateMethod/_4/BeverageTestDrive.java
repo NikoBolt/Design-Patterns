51e946c494d1113faeb26ac1bb9961f6894cd601
NikoBolt <docnikbolt@gmail.com>
2020-09-20 20:02:21 +0300

    Тест шаблонного метода

---
+++ src/com/company/_8_TemplateMethod/_4/BeverageTestDrive.java
@@ -0,0 +1,15 @@
+package com.company._8_TemplateMethod._4;
+
+public class BeverageTestDrive {
+	public static void main(String[] args) {
+ 
+		Tea tea = new Tea();
+		Coffee coffee = new Coffee();
+ 
+		System.out.println("\nMaking tea...");
+		tea.prepareRecipe();
+ 
+		System.out.println("\nMaking coffee...");
+		coffee.prepareRecipe();
+	}
+}
