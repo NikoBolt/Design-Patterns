8df30aa3367c37b5c8c247a5d5d2294d76c16c4a
NikoBolt <docnikbolt@gmail.com>
2020-10-10 16:23:01 +0300

    Паттерн компоновщик

---
+++ src/com/company/_9_2_composite/_2/Menu.java
@@ -0,0 +1,46 @@
+package com.company._9_2_composite._2;
+
+import java.util.ArrayList;
+
+public class Menu extends MenuComponent {
+    ArrayList<MenuComponent> menuComponents = new ArrayList<MenuComponent>();
+    String name;
+    String description;
+
+    public Menu(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public void add(MenuComponent menuComponent) {
+        menuComponents.add(menuComponent);
+    }
+
+    @Override
+    public void remove(MenuComponent menuComponent) {
+        menuComponents.remove(menuComponent);
+    }
+
+    @Override
+    public MenuComponent getChild(int i) {
+        return menuComponents.get(i);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("\n" + getName());
+        System.out.println(" " + getDescription());
+        System.out.println("--------------------");
+    }
+}
---
+++ src/com/company/_9_2_composite/_2/MenuComponent.java
@@ -0,0 +1,39 @@
+package com.company._9_2_composite._2;
+
+public abstract class MenuComponent {
+
+    // Комбинационные методы
+    public void add(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void remove(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    public MenuComponent getChild(int i) {
+        throw new UnsupportedOperationException();
+    }
+
+    // Методы операций
+    public String getName() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getDescription() {
+        throw new UnsupportedOperationException();
+    }
+
+    public double getPrice() {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean isVegetarian() {
+        throw new UnsupportedOperationException();
+    }
+
+    // Реализуется и в Menu и в MenuItem
+    public void print() {
+        throw new UnsupportedOperationException();
+    }
+}
