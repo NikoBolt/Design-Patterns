c0558c0325eb43b6348cfe9e9af2a5ab99069ff3
NikoBolt <docnikbolt@gmail.com>
2020-12-13 15:58:55 +0300

    Паттерн Заместитель. Динамический заместитель. Защитный зам

---
+++ src/com/company/_11_Proxy/_5_Security_Proxy/Dynamyc_Proxy/_5_1/PersonBeanImpl.java
@@ -0,0 +1,32 @@
+package com.company._11_Proxy._5_Security_Proxy.Dynamyc_Proxy._5_1;
+
+public class PersonBeanImpl implements PersonBean {
+	String name;
+	String gender;
+	String interests;
+	int rating;
+	int ratingCount = 0;
+
+	// get-теры
+	public String getName() { return name; }
+  	public String getGender() { return gender; }
+	public String getInterests() { return interests; }
+
+	// Вычисляет и возвращает среднюю оценку
+	public int getHotOrNotRating() {
+		if (ratingCount == 0) return 0;
+		return (rating/ratingCount);
+	}
+  
+ 	// Set-теры
+	public void setName(String name) { this.name = name;}
+ 	public void setGender(String gender) { this.gender = gender; }
+ 	public void setInterests(String interests) { this.interests = interests; }
+
+ 	// увеличивает количество оценок и накапливает сумму оченок
+	public void setHotOrNotRating(int rating) {
+		this.rating += rating;	
+		ratingCount++;
+	}
+
+}
